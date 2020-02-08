50478b7a82267cf4fdc35299a9b9cb8ebe4caa80
Fernando Espíndola <fer.esp@gmail.com>
2020-02-08 01:43:58 -0300

    Final solution for the jarden casting

--- codewars/kata/fundamentals/jaden-casing-strings-5390bac347d09b7da40006f6/other-solutions.py
+++ codewars/kata/fundamentals/jaden-casing-strings-5390bac347d09b7da40006f6/other-solutions.py
@@ -1,41 +1,22 @@
 # -------------------------------------------------------------------------------------------------
 
-import operator
-def persistence(n):
-    i = 0
-    while n>=10:
-        n=reduce(operator.mul,[int(x) for x in str(n)],1)
-        i+=1
-    return i
+def toJadenCase(string):        
+    return " ".join(w.capitalize() for w in string.split())
 
 # -------------------------------------------------------------------------------------------------
 
-def persistence(n):
-    nums = [int(x) for x in str(n)]
-    sist = 0
-    while len(nums) > 1:
-        newNum = reduce(lambda x, y: x * y, nums)
-        nums = [int(x) for x in str(newNum)]
-        sist = sist + 1
-    return sist
+import string
+
+def toJadenCase(NonJadenStrings):
+    return string.capwords(NonJadenStrings)
 
 # -------------------------------------------------------------------------------------------------
 
-def persistence(n):
-    n = str(n)
-    count = 0
-    while len(n) > 1:
-        p = 1
-        for i in n:
-            p *= int(i)
-        n = str(p)
-        count += 1
-    return count
+import string
+toJadenCase = string.capwords
 
 # -------------------------------------------------------------------------------------------------
 
-from operator import mul
-def persistence(n):
-    return 0 if n<10 else persistence(reduce(mul,[int(i) for i in str(n)],1))+1
+from string import capwords as toJadenCase
 
 # -------------------------------------------------------------------------------------------------
