c53f938dfa80de49d5d34b7d4ee2b5b0326fa810
Fernando Espíndola <fer.esp@gmail.com>
2020-02-08 11:19:06 -0300

    initial problem description.

---
+++ codewars/kata/fundamentals/are-they-the-same-550498447451fbbd7600041c/code.py
@@ -0,0 +1,14 @@
+def comp(array1, array2):
+    pass
+	
+
+
+if __name__== "__main__":
+    import codewars_test as test
+
+    # Sample Tests
+    a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+    a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+    test.assert_equals(comp(a1, a2), True)
+
+    # Test Case
